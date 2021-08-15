19fb424a5b025b3c2c7519ad9aa0f4153222a012
Lucky Rai <lucky.rai@waveaxis.co.in>
2021-08-15 12:45:30 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ComponentDetails from './CommentDetails';
+import faker from 'faker'
+import ApprovalCard from './ApprovalCard'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div className="ui container comments">
+    <ApprovalCard><div>
+      <h4>!Warning</h4>
+          Are you sure you want to do this ?
+    </div></ApprovalCard>
+    <ApprovalCard> <ComponentDetails author="Ram" timeAgo="Today at 4pm" content="Hi I m Ram" avatar={faker.image.avatar()} />
+
+    </ApprovalCard>
+
+    <ApprovalCard><ComponentDetails author="Mohan" timeAgo="Today at 6pm" content="Hi I m Mohan" avatar={faker.image.avatar()} /></ApprovalCard>
+
+    <ApprovalCard> <ComponentDetails author="Anil" timeAgo="Today at 8pm" content="Hi I am Anil" avatar={faker.image.avatar()} /></ApprovalCard>
+
+
+
+
+  </div>,
   document.getElementById('root')
 );
 
